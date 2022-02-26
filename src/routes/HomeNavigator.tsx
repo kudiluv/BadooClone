2719a48bcc7f65ccbc88107676a272acea9f98b9
Igor <kudlaev.igor@gmail.com>
2022-02-26 18:43:57 +0300

    fix: renamed folders structure

--- src/routes/HomeNavigator.tsx
+++ src/routes/HomeNavigator.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import ProfileScreen from '../screens/profile.screen/ProfileScreen';
-import ChatsScreen from '../screens/chats.screen/ChatsScreen';
+import ProfileScreen from '../screens/profile/ProfileScreen';
+import ChatsScreen from '../screens/chats/ChatsScreen';
+import SearchScreen from '../screens/search/SearchScreen';
 
 const Tab = createBottomTabNavigator();
 
 const HomeNavigator = () => {
   return (
     <Tab.Navigator>
+      <Tab.Screen name="Search" component={SearchScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
       <Tab.Screen name="Chats" component={ChatsScreen} />
       <Tab.Screen name="Likes" component={ProfileScreen} />
